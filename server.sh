0ed1ca917b076277e9b9cda1b72dae18ce62d11b
陈锦铸 <15818349+emt_486@user.noreply.gitee.com>
2025-05-27 23:47:17 +0800

    update

--- server.sh
+++ server.sh
@@ -7,25 +7,25 @@ sudo apt install qemu-kvm qemu-system-riscv64 xz-utils curl podman sshpass -y
 echo "ready to pull openeuler 24 RV image"
 
 if [[ ! -e openEuler-24.03-LTS-riscv64.qcow2.xz ]]; then
-        echo "pulling openeuler 24 RV"
-        curl -o openEuler-24.03-LTS-riscv64.qcow2.xz https://dl-cdn.openeuler.openatom.cn/openEuler-24.03-LTS/virtual_machine_img/riscv64/openEuler-24.03-LTS-riscv64.qcow2.xz
+    echo "pulling openeuler 24 RV"
+    curl -o openEuler-24.03-LTS-riscv64.qcow2.xz https://dl-cdn.openeuler.openatom.cn/openEuler-24.03-LTS/virtual_machine_img/riscv64/openEuler-24.03-LTS-riscv64.qcow2.xz
 fi
 
 if [[ ! -e RISCV_VIRT_VARS.fd ]]; then
-        echo "pulling RISCV_VIRT_VARS.fd"
-        curl -o RISCV_VIRT_VARS.fd https://dl-cdn.openeuler.openatom.cn/openEuler-24.03-LTS/virtual_machine_img/riscv64/RISCV_VIRT_VARS.fd
+    echo "pulling RISCV_VIRT_VARS.fd"
+    curl -o RISCV_VIRT_VARS.fd https://dl-cdn.openeuler.openatom.cn/openEuler-24.03-LTS/virtual_machine_img/riscv64/RISCV_VIRT_VARS.fd
 fi
 
 if [[ ! -e RISCV_VIRT_CODE.fd ]]; then
-        echo "pulling RISCV_VIRT_CODE.fd"
-        curl -o RISCV_VIRT_CODE.fd https://dl-cdn.openeuler.openatom.cn/openEuler-24.03-LTS/virtual_machine_img/riscv64/RISCV_VIRT_CODE.fd
+    echo "pulling RISCV_VIRT_CODE.fd"
+    curl -o RISCV_VIRT_CODE.fd https://dl-cdn.openeuler.openatom.cn/openEuler-24.03-LTS/virtual_machine_img/riscv64/RISCV_VIRT_CODE.fd
 fi
 
 echo "openeuler 24 RV image exists"
 
 if [[ ! -e openEuler-24.03-LTS-riscv64.qcow2 ]]; then
-        echo "extract downloaded image"
-        xz -dk openEuler-24.03-LTS-riscv64.qcow2.xz
+    echo "extract downloaded image"
+    xz -dk openEuler-24.03-LTS-riscv64.qcow2.xz
 fi
 
 echo "extract image ok"
@@ -34,9 +34,17 @@ echo "ready to start virtual machine"
 
 qemu-system-riscv64 -machine virt,pflash0=pflash0,pflash1=pflash1,acpi=off -smp 12 -m 4G -blockdev node-name=pflash0,driver=file,read-only=on,filename=RISCV_VIRT_CODE.fd -blockdev node-name=pflash1,driver=file,filename=RISCV_VIRT_VARS.fd -drive file=openEuler-24.03-LTS-riscv64.qcow2,format=qcow2,id=hd0,if=none -device virtio-vga -device virtio-rng-device -device virtio-blk-device,drive=hd0,bootindex=1 -device virtio-net-device,netdev=usernet -netdev user,id=usernet,hostfwd=tcp::2222-:22,hostfwd=tcp::9000-:9000,hostfwd=tcp::9001-:9001 -device qemu-xhci -usb -device usb-kbd -device usb-tablet -display none -daemonize
 
-sleep 60
+echo $?
 
-echo "virtual machine started"
+sleep 120
+
+n="$(netstat -lntp | grep 222 | wc -l)"
+if [[ n -gt 1 ]]; then
+    echo "Virtual machine is running"
+else
+    echo "Virtual machine is not running"
+    exit 1
+fi
 
 sshpass -p 'openEuler12#$' scp -o StrictHostKeyChecking=no -P 2222 ./run.sh root@localhost:/root/run.sh
 
