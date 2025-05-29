13369ace692f98eb36e48a47da3dcee1dc177f14
陈锦铸 <15818349+emt_486@user.noreply.gitee.com>
2025-05-29 23:05:17 +0800

    update

--- run.sh
+++ run.sh
@@ -14,62 +14,62 @@ chronyc makestep
 useradd -m -s /bin/bash john
 echo "john:testtesttest" | chpasswd
 
-su - john -c "
-    cd /home/john
+su john <<EOF
+cd /home/john
 
-    whoami
+whoami
 
-    pwd
+pwd
 
-    git clone https://github.com/minio/minio.git
-    cd minio
-    git checkout RELEASE.2025-04-22T22-12-26Z
+git clone https://github.com/minio/minio.git
+cd minio
+git checkout RELEASE.2025-04-22T22-12-26Z
 
-    curl https://dl.google.com/go/go1.24.3.linux-riscv64.tar.gz -o go1.24.3.linux-riscv64.tar.gz
-    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.3.linux-riscv64.tar.gz
-    export PATH=$PATH:/usr/local/go/bin
-    export PATH=$PATH:$(go env GOPATH)/bin
+curl https://dl.google.com/go/go1.24.3.linux-riscv64.tar.gz -o go1.24.3.linux-riscv64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.3.linux-riscv64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$(go env GOPATH)/bin
 
-    go version
+go version
 
-    go env
+go env
 
-    go mod tidy
+go mod tidy
 
-    sed -i 's/loongarch64)/loongarch64 | riscv64)/g' buildscripts/checkdeps.sh
+sed -i 's/loongarch64)/loongarch64 | riscv64)/g' buildscripts/checkdeps.sh
 
-    sed -i 's/timeout=10m/timeout=2000m/' Makefile
+sed -i 's/timeout=10m/timeout=2000m/' Makefile
 
-    sed -i '/@MINIO_API_REQUESTS_MAX=10000 CGO_ENABLED=0 go test -v -tags kqueue,dev .\/.../ s/$/ -timeout 0/' Makefile
+sed -i '/@MINIO_API_REQUESTS_MAX=10000 CGO_ENABLED=0 go test -v -tags kqueue,dev .\/.../ s/$/ -timeout 0/' Makefile
 
-    free -mh
-    df -mh
-    dd if=/dev/zero of=/swapfile count=8192 bs=1M
-    chmod 600 /swapfile
-    mkswap /swapfile
-    swapon /swapfile
+free -mh
+df -mh
+dd if=/dev/zero of=/swapfile count=8192 bs=1M
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
 
-    make
+make
 
-    echo "finished building minio"
+echo "finished building minio"
 
-    echo "ready to run make test"
-    make test
+echo "ready to run make test"
+make test
 
-    echo "make test finished"
+echo "make test finished"
 
-    echo "ready to run mint"
+echo "ready to run mint"
 
-    file minio
+file minio
 
-    chronyc makestep
-    date
+chronyc makestep
+date
 
-    whoami
+whoami
 
-    pwd
+pwd
 
-    ./minio server ~/data --console-address ":9001" >minio.log 2>&1 &
+./minio server ~/data --console-address ":9001" >minio.log 2>&1 &
 
-    sleep 10
-“
+sleep 10
+EOF
