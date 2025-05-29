2a43acc0656d0e0e5bc4104476c5839182964593
陈锦铸 <15818349+emt_486@user.noreply.gitee.com>
2025-05-29 23:21:42 +0800

    update

--- run.sh
+++ run.sh
@@ -11,8 +11,20 @@ systemctl start chronyd
 date
 chronyc makestep
 
+curl https://dl.google.com/go/go1.24.3.linux-riscv64.tar.gz -o go1.24.3.linux-riscv64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.3.linux-riscv64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$(go env GOPATH)/bin
+
 useradd -m -s /bin/bash john
 echo "john:testtesttest" | chpasswd
+
+free -mh
+df -mh
+dd if=/dev/zero of=/swapfile count=8192 bs=1M
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
 
 su john <<EOF
 cd /home/john
@@ -25,11 +37,6 @@ git clone https://github.com/minio/minio.git
 cd minio
 git checkout RELEASE.2025-04-22T22-12-26Z
 
-curl https://dl.google.com/go/go1.24.3.linux-riscv64.tar.gz -o go1.24.3.linux-riscv64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.3.linux-riscv64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-export PATH=$PATH:$(go env GOPATH)/bin
-
 go version
 
 go env
@@ -41,13 +48,6 @@ sed -i 's/loongarch64)/loongarch64 | riscv64)/g' buildscripts/checkdeps.sh
 sed -i 's/timeout=10m/timeout=2000m/' Makefile
 
 sed -i '/@MINIO_API_REQUESTS_MAX=10000 CGO_ENABLED=0 go test -v -tags kqueue,dev .\/.../ s/$/ -timeout 0/' Makefile
-
-free -mh
-df -mh
-dd if=/dev/zero of=/swapfile count=8192 bs=1M
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
 
 make
 
@@ -69,7 +69,7 @@ whoami
 
 pwd
 
-./minio server ~/data --console-address ":9001" >minio.log 2>&1 &
+./minio server /home/john/minio/data --console-address ":9001" >minio.log 2>&1 &
 
 sleep 10
 EOF
